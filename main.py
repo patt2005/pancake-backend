81d41475ef714d1df9b250fc8761b473b63f2a3e
Peter Grigor <petrupewdiepie@gmail.com>
2025-07-16 21:22:07 +0300

    some chnages

--- main.py
+++ main.py
@@ -11,3 +11,8 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+
+@app.get("/api-url")
+async def get_api_url():
+    return {"apiUrl": "https://pancake-backend-164860087792.europe-west1.run.app"}
